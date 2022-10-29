775afff1937c785bc410aaa21879b9a65d07069d
NazarYavor26 <92759124+NazarYavor26@users.noreply.github.com>
2022-10-29 14:34:00 +0300

    created routing for components

--- my-guide/src/app/app-routing.module.ts
+++ my-guide/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { ExcursionComponent } from './pages/excursion/excursion.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'excursion', component:ExcursionComponent},
+  {path:'about', component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
